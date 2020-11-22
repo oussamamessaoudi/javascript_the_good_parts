809bb5fdb0e096a3a136e2847d918f53b2aaacd4
oussamamessaoudi <oussm.messaoudi@gmail.com>
2020-11-22 16:06:08 +0100

    Notes + script of Chapter 4:Functions.

--- readme.md
+++ readme.md
@@ -18,7 +18,6 @@ expressive object literal notation
 ```
 Based on global variables
 ```
-
 ## CHAPTER 2: Grammar
 ### Whitespace
 Whitespace is **USUALLY** insignificant
@@ -118,8 +117,156 @@ var MY_APP = {};
 MY_APP.stooge = {};
 //...
 ```
+## Chapter 4: Functions
+- The best thing about JavaScript is the implementation of functions.
+- used for code reuse, information hiding, composition Functions.
+### Function Objects
+- JavaScript are objects
+- Function object are linked by a prototype to Function.prototype which is linked to Object.prototype.
+- Every function created with the context and the code.
+- Every function contains a constructor function in the prototype.
+### Function Literal
+- Function has a name is not is called anonymous.
+- a function has access to parameters and variables
+- inner function has access to the outer context called **closure**
+### Invocation
+- Invoking a function suspends the execution of the current function.
+- every function has two additional params: **this**, **arguments**
+- **this** is determined by the *invocation pattern*.
+#### The method Invocation Pattern 
+A function is stored as a property of an object it's called a *method*.
+- **this** is bound to that object.
+- this binding of this to the object happens at invocation time (late or lazy).
+#### The function Invocation Pattern
+function is invoked without being the property of an object.
+- **this** is bound to the global var.
+- even for inner function the **this** is bounded to the global var, use **that**.
+```javascript
+myObject.double = function(){
+    var that = this;
+    var helper = function() {
+        that.value = add(that.value, that.value);
+    }
+    helper();
+}
+myObject.double();
+```
+#### The Constructor Invocation Pattern
+- Javascript is *prototypal* inheritance language. 
+- Objects can inherit properties directly from other objects.
+- The language is class-free.
+- the function is invoked the *new* prefix.
+- *this* is bounded to the created object.
+- Functions that are intended to be called with *new* are called **constructor**.
+- Convention is PascalNotation (Uppercase)
+#### The Apply Invocation Pattern
+- Functions can have methods
+- apply(newThis, args)
+### Arguments
+- Access to all the args that were supplied with the invocation, including excess args.
+- So we can create a function that take an unspecified number of parameters.
+- arguments is not really an array, it's an array-like object.
+- arguments has a length property but lacks all the array methods.
+### Return
+- We use return to return a value or return early.
+- A function returns always a value (if not undefined is returned).
+- func invoked by *new* operator, and the return type is an object, then the object else it returns this. 
+### Exception
+- using *throw* an object that contains a *name* and a *message*.
+- using try catch(e).
+- has a single catch close.
+### Augmenting Types
+```javascript
+/*
+    Augmenting Function.prototype with a *method* method. 
+    We no longer have to type the name of the *prototype* proprety
+*/
+Function.prototype.method = function (name, func) {
+    if(!this.prototype[name]){
+        this.prototype[name] = func;
+        return this;
+    }
+}
+```
+### Recursion
+A *recursive* is a function that calls itself, either or indirectly.
+- JavaScript does not currently provide *tail recursion optimization* (the replacement of the recursion by a loop).
+### Scope
+- Scope control the visibility and lifetimes of variables and parameters.
+- JavaScript has not block scope.
+- JavaScript does have function scope.
+- It is recommended to declare all vars used on the top of a func.
+### Closure
+- Inner function has access to the **real** params and vars (*not a copy*) of the outer functions.
+```javascript
+//The inner function has a longer lifetime than its outer func
+var myObject = (function () {
+    var value = 0;
+    
+    return {
+        increment: function (inc) {
+            value += typeof inc === 'number' ? inc : 1;
+        },
+        getvalue : function () {
+            return value;
+        }
+    }
+})()
+```
+### Callbacks
+- is a function as param for another function to call it within the method (asynchronous).
+### Module
+- module is a function or object that hide its implementation.
+````javascript
+var serial_maker = function (startSeq) {
+    //Cannot be modified from the outside of the function (private)
+    var seq = startSeq;
+    return {
+        gensym: function (){
+            var result = 'G' +seq;
+            seq += 1;
+            return result;
+        }
+    }
+}
+````
+### Cascade
+Cascading is calling multiple on the same instance.
+````javascript
+getElement('myBoxDiv').move(350, 150).width(100).color('red').appendText("Cascading ...")
+````
+### Curry
+*Currying* allows to produce a new function by combining params;
+````javascript
+//Curry method
+Function.method('curry', function (){
+    var slice = Array.prototype.slice, args = slice.apply(arguments), that = this;
+    return function (){
+        //Argument is not a real array concat doesn't work == args.concat(arguments) Error
+        return that.apply(that, args.concat(slice.apply(arguments)));
+    }
+})
+var increment_by_one = add.curry(1);
+increment_by_one(5) // return 6
+````
+### Memoization
+The use of an object to remember the result of the same input to avoid unnecessary work.
+````javascript
+//Making a global Memoize function
+var memoizer = function (memo, formula) {
+    var recur = function (n){
+        var result = memo[n];
+        if(typeof result !== "number"){
+            result = formula(recur, n);
+            memo[n] = result;
+        }
+        return result;
+    };
+    return recur;
+};
+var making_fib = memoizer([0, 1], function (recur, n) {
+    return recur(n-1) + recur(n-2);
+})
+````
 
 
-
- 
- 
--- script.js
+++ script.js
@@ -80,3 +80,260 @@ console.log("*** Delete ***")
 console.log("another_stooge.nickname:", another_stooge.nickname, "is equal to Moe");
 delete another_stooge.nickname;
 console.log("another_stooge.nickname:", another_stooge.nickname, "is equal to Curly");
+
+/*
+    Chapter 4: Functions
+ */
+//Literal Function
+var add = function (a, b) {
+    return a + b;
+}
+// Method Invocation Pattern
+console.log("*** Method Invocation Pattern ***")
+var myObject = {
+    value : 0,
+    increment: function (inc){
+        this.value += typeof inc === 'number' ? inc : 1;
+    }
+}
+myObject.increment();
+console.log("Value: ", myObject.value , "is equal to 1")
+myObject.increment(3)
+console.log("Value: ", myObject.value , "is equal to 4")
+// Function Invocation Pattern
+console.log("*** The Function Invocation Pattern ***")
+var sum = add(3, 4);
+myObject.double = function(){
+    var that = this;
+    var helper = function() {
+        that.value = add(that.value, that.value);
+    }
+    helper();
+}
+myObject.double();
+console.log("Value: ", myObject.value , "is equal to 8")
+console.log("*** The Constructor Invocation Pattern ***")
+var Quo = function (status){
+    this.status = status
+}
+Quo.prototype.get_status = function () {
+    return this.status;
+}
+var myQuo = new Quo("confused");
+console.log("Status:", myQuo.get_status())
+console.log("*** The Apply Invocation Pattern ***")
+var array = [3, 4]
+var sumOfArray = add.apply(null, array);
+console.log("Sum of array:", sumOfArray, "is equal to 7");
+var statusObject = {
+    status: 'A-OK'
+};
+var status = Quo.prototype.get_status.apply(statusObject);
+//use statusObject doesn't inherit  the Quo.prototype
+console.log("Status:", status, "isEqual to " + statusObject.status)
+console.log("*** Arguments ***")
+var sumOfArgs = function (){
+    var i, sum = 0;
+    for(i=0; i<arguments.length; i += 1){
+        sum += arguments[i];
+    }
+    return sum;
+}
+console.log("Sum:",sumOfArgs(4, 8, 15, 16, 23), "is Equal to 66")
+console.log("*** Augmenting Types ***")
+Function.prototype.method = function (name, func){
+    if(!this.prototype[name]){
+        this.prototype[name] = func
+        return this;
+    }
+}
+Number.method('integer', function () {
+    return Math[this < 0 ? 'ceil' : 'floor'](this)
+})
+console.log('To integer method:', (-10/3).integer());
+//Remove space from the start and end of a string
+String.method('trim', function (){
+    return this.replace(/^\s+|\s+$/g, '')
+})
+var toTrim = "   trim    "
+console.log('Trim string:"', toTrim, '"=>"'+toTrim.trim(), '"');
+console.log("*** Recursion ***")
+var hanoi = function hanoi(disc, src, aux, dst){
+    if(disc > 0){
+        hanoi(disc-1, src, dst, aux);
+        console.log('Move disc '+ disc + ' from ' + src + ' to ' + dst);
+        hanoi(disc-1, aux, src, dst);
+    }
+}
+hanoi(3, 'Src', 'Aux', 'Dst');
+var walk_the_DOM = function walk(node, func) {
+    func(node);
+    node = node.firstChild;
+    while (node) {
+        walk(node, func);
+        node = node.nextSibling
+    }
+}
+var getElementsByAttribute = function (att, value) {
+    var results = [];
+
+    walk_the_DOM(document.body, function (node){
+        var actual = node.nodeType === 1 && node.getAttribute(att);
+        if(typeof actual === 'string' &&
+            (actual === value || typeof value !== 'string')){
+            results.push(node);
+        }
+    });
+
+    return results;
+}
+var factorial = function factorial(i, a){
+    a = a || 1;
+    if (i < 2) return a;
+    return factorial(i-1, a * i)
+}
+console.log("factorial("+factorial(4)+")");
+console.log("*** Closure ***")
+var myObject = (function () {
+    var value = 0;
+
+    return {
+        increment: function (inc) {
+            value += typeof inc === 'number' ? inc : 1;
+        },
+        getValue : function () {
+            return value;
+        }
+    }
+})()
+var Quo = function (status){
+    return {
+        get_status: function (){
+            return status;
+        }
+    }
+}
+console.log("Quo('amazed'):", Quo('amazed').get_status());
+var fade = function (node) {
+    var level = 1;
+    var step = function (){
+        var hex = level.toString(16);
+        node.style.backgroundColor = '#FFFF'+hex+hex;
+        if(level < 15){
+            level += 1;
+            setTimeout(step, 100);
+        }
+    };
+    setTimeout(step, 100);
+}
+fade(document.body);
+//BAD EXAMPLE
+var add_the_handlers = function (nodes){
+    var i;
+    for (i = 0; i< nodes.length; i += 1){
+        nodes[i].onclick = function (){
+            // the i is not a copy (so it's value will always be nodes.length)
+            alert(i);
+        }
+    }
+}
+// BETTER EXAMPLE
+var add_the_handlers = function (nodes){
+    var helper = function (i){
+        return function (e){
+            alert(i);
+        };
+    };
+    var i;
+    for(i = 0; i < nodes.length; i += 1){
+        nodes[i].onclick = helper(i);
+    }
+}
+console.log("*** Module ***")
+String.method('deentityify', function (){
+    //Entity cannot be modified from outside
+    var entity = {
+        quot: '"',
+        lt: '<',
+        gt: '>'
+    };
+    var that = this;
+    return (function () {
+        return that.replace(/&([^&;]+);/g,
+            function (a, b) {
+                var r  = entity[b];
+                return (typeof r === 'string') ? r : a;
+        })
+    })()
+})
+console.log("'&lt;&quot;&gt'.deentityify() =>", '&lt;&quot;&gt;'.deentityify());
+var serial_maker = function () {
+    //Cannot be modified from the outside of the function
+    var prefix = '';
+    var seq = 0;
+    return {
+        set_prefix: function (p){
+            prefix = String(p);
+        },
+        set_seq: function (s){
+            seq = s;
+        },
+        gensym: function (){
+            var result = prefix +seq;
+            seq += 1;
+            return result;
+        }
+    }
+}
+var seqer = serial_maker();
+seqer.set_prefix('Q');
+seqer.set_seq(1000);
+console.log('generale a new serial number:', seqer.gensym(), seqer.gensym());
+console.log("*** Curry ***")
+Function.method('curry', function (){
+    var slice = Array.prototype.slice, args = slice.apply(arguments), that = this;
+    return function (){
+        //Argument is not a real array concat doesn't work == args.concat(arguments) Error
+        return that.apply(that, args.concat(slice.apply(arguments)));
+    }
+})
+var increment_by_one = add.curry(1);
+console.log("increment_by_one(5):", increment_by_one(5))
+console.log("*** Memoization ***")
+var fibonacci = function (n){
+    return n < 2 ? n : fibonacci(n-1) + fibonacci(n-2);
+}
+// Memoize
+var fibonacci_memoize = (function (){
+    var memo = [0, 1];
+    var fib = function (n) {
+        var result = memo[n];
+        if (typeof result != "number"){
+            result = fib(n-1) + fib(n-2);
+            memo[n] = result;
+        }
+        return result;
+    }
+    return fib;
+})();
+console.log("fibonacci_memoize(10):", fibonacci_memoize(10))
+//Making a global Memoize function
+var memoizer = function (memo, formula) {
+    var recur = function (n){
+        var result = memo[n];
+        if(typeof result !== "number"){
+            result = formula(recur, n);
+            memo[n] = result;
+        }
+        return result;
+    };
+    return recur;
+};
+var making_fib = memoizer([0, 1], function (recur, n) {
+    return recur(n-1) + recur(n-2);
+})
+console.log("making_fib(10):", making_fib(10))
+var making_fact = memoizer([1, 1], function (recur, n){
+    return n* recur(n-1);
+})
+console.log("making_fact(10):", making_fact(10))
